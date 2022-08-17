a6323df39d0f1a58b0d3a27a99372fa6be375977
GregT <taylor.gregory2@gmail.com>
2022-08-17 17:08:37 -0600

    shoe controller updated

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,2 +1,31 @@
 class ShoesController < ApplicationController
+    def index
+        render json: Shoe.all
+    end
+
+    def show
+        render json: Shoe.find_by(id: params[:id])
+    end
+
+    def update
+        shoe = Shoe.find_by(id: params[:id])
+        shoe.update(params)
+    end
+
+    def create
+        shoe = Shoe.create(shoe_params)
+        render json: shoe, status: :created
+    end
+
+    def delete
+        shoe = Shoe.find_by(id: params[:id])
+        shoe.delete
+        head :no_content
+    end
+
+    private
+
+    def shoe_params
+        params.permit(:model_num, :name, :brand_id, :in_stock, :price )
+    end
 end
