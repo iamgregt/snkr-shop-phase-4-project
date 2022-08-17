d117c83ac65ff58e1cbf71c103ae2fdb480d1853
GregT <taylor.gregory2@gmail.com>
2022-08-17 16:13:00 -0600

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :shoes
+
+    validates :username, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :balance, numericality: {greater_than_or_equal_to: 0 }
+    validates :shoe_size, numericality: {greater_than: 3, less_than: 16}
+
 end
