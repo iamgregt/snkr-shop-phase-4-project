e70bfbf53064022b2866b56bdc924763ff2abf72
GregT <taylor.gregory2@gmail.com>
2022-08-18 20:17:52 -0600

    cors added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from "react";
 function App() {
+
+  const [shoes, setShoes] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/shoes')
+    .then(r => r.json())
+    .then(shoes => setShoes(shoes))
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {shoes.map((s) => {
+            return <li>{s}</li>
+          })}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
