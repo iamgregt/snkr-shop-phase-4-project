13706e3d4f0cb3f905a71aa2ba0865528e6e19c3
GregT <taylor.gregory2@gmail.com>
2022-08-21 11:34:19 -0600

    Stores added to SC

--- client/src/StoresContainer.js
+++ client/src/StoresContainer.js
@@ -1,2 +1,17 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Stores from "./Stores";
 
+function StoresContainer() {
+
+
+    return(
+        <>
+        <div id="stores-container">
+            <Stores />
+        </div>
+        </>
+    )
+
+}
+
+export default StoresContainer
