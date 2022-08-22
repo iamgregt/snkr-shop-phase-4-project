c8fa63aa0ca45de840e737412384337ee8f00b24
GregT <taylor.gregory2@gmail.com>
2022-08-21 21:59:16 -0600

    react router installed

--- client/src/Stores.js
+++ client/src/Stores.js
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import  Col  from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container"
 import Row from 'react-bootstrap/Row';
 
 
--- client/src/StoresContainer.js
+++ client/src/StoresContainer.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from "react";
 import Stores from "./Stores";
 import Container from "react-bootstrap/Container"
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function StoresContainer() {
 
